6e5837e1852372f0c5dbe4bdfb8f9fb3ae4e672b
Peter <peev.petar@gmail.com>
2018-04-15 11:51:54 +0300

    das

---
+++ app.js
@@ -0,0 +1,139 @@
+$(()=>{
+
+    const app = Sammy('#main',function () {
+            this.use('Handlebars','hbs');
+            this.get('index.html',function () {
+              this.redirect('#/home');
+              return;
+            });
+            this.get('#/home', getWelcomePage);
+            this.get('#/login',getWelcomePage);
+            this.get('#/register', (ctx) => {
+                if (!auth.isAuth()) {
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs'
+                    }).then(function () {
+                        this.partial('./templates/register.hbs')
+                    })
+                } else {
+                    ctx.redirect('#/feed')
+                }
+            });
+            this.post('#/register',(ctx)=>{
+                let username = ctx.params.username;
+                let password = ctx.params.password;
+                let repeatPass = ctx.params.repeatPass;
+
+                let usernameMatch = /^[A-za-z]{5,}$/.test(username);
+                let passMatch = /^[A-za-z\d]{6,}$/.test(password);
+
+                if (!usernameMatch) {
+                    displayNotification.showError('Username should be at least 5 characters long');
+
+                } else if (!passMatch) {
+                    displayNotification.showError('Password should be at least 6 characters long')
+
+                } else if (password !== repeatPass) {
+                    displayNotification.showError('Passwords must match')
+                } else {
+                    auth.register(username, password)
+                        .then((userData) => {
+                            auth.saveSession(userData);
+                            displayNotification.showInfo('User registration successful!');
+                            ctx.redirect('#/feed');
+                        })
+                        .catch(displayNotification.handleError);
+                }
+            });
+            this.post('#/login',(ctx)=>{
+                let username = ctx.params.username;
+                let password = ctx.params.password;
+
+                if(username === '' || password === ''){
+                displayNotification.showError('username and password can`t be empty ')
+                }else{
+                auth.login(username,password)
+                    .then((userData)=>{
+                    auth.saveSession(userData);
+                    displayNotification.showInfo('Login Successful.');
+                    ctx.redirect('#/feed');
+                }).catch(displayNotification.handleError)
+                }
+
+            });
+            this.get('#/logout',(ctx) =>{
+               auth.logout().then(()=> {
+                   sessionStorage.clear();
+                   ctx.redirect('#/home')
+               }).catch(displayNotification.handleError)
+            });
+            this.get('#/feed',(ctx)=>{
+                if (auth.isAuth()) {
+
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs',
+                        menu: './templates/common/menu.hbs'
+                    }).then(function () {
+
+                        let info = service.getStats(sessionStorage.getItem('username')).then(function (value) {
+                            console.log(value)
+                        });
+                        service.getChirps().then(data =>{
+                            ctx.username = sessionStorage.getItem('username');
+                             ctx.chirps = data;
+                             data.forEach(p => {
+                              p.date = calcTime(p._kmd.ect);
+                             });
+                             ctx.chirpsCount = info[0];
+                             ctx.following = info[1];
+                             ctx.followers = info[2];
+
+                            this.partial('./templates/feed.hbs')
+                        })
+
+                    })
+                }
+            });
+
+
+
+
+        function getWelcomePage(ctx) {
+
+            if (!auth.isAuth()) {
+                ctx.loadPartials({
+                    header: './templates/common/header.hbs',
+                    footer: './templates/common/footer.hbs'
+                }).then(function () {
+                    this.partial('./templates/login.hbs')
+                })
+            } else {
+                ctx.redirect('#/feed')
+            }
+        }
+        function calcTime(dateIsoFormat) {
+            let diff = new Date - (new Date(dateIsoFormat));
+            diff = Math.floor(diff / 60000);
+            if (diff < 1) return 'less than a minute';
+            if (diff < 60) return diff + ' minute' + pluralize(diff);
+            diff = Math.floor(diff / 60);
+            if (diff < 24) return diff + ' hour' + pluralize(diff);
+            diff = Math.floor(diff / 24);
+            if (diff < 30) return diff + ' day' + pluralize(diff);
+            diff = Math.floor(diff / 30);
+            if (diff < 12) return diff + ' month' + pluralize(diff);
+            diff = Math.floor(diff / 12);
+            return diff + ' year' + pluralize(diff);
+
+            function pluralize(value) {
+                if (value !== 1) return 's';
+                else return '';
+            }
+        }
+    });
+
+
+    app.run();
+});
---
+++ services/service.js
@@ -0,0 +1,163 @@
+let service = (() => {
+
+    let appdata = 'appdata';
+    let authKey = 'kinvey';
+    let user = 'user';
+
+
+
+    function getChirps() {
+        let subs = sessionStorage.getItem('subscriptions');
+        let endpoint = `chirps?query={"author":{"$in": ${subs}}}&sort={"_kmd.ect": 1}`;
+          return  remote.get(appdata,endpoint,authKey)
+
+    }
+
+    function createChirp(text) {
+        let endpoint = 'chirps';
+        const data = {
+            author:sessionStorage.getItem('username'),
+            text: text
+        };
+
+        return remote.post(appdata,endpoint,authKey,data)
+
+    }
+    
+    function deleteChirp(id) {
+        let endpoint = 'chirps/' + id;
+        return remote.remove(appdata,endpoint,authKey)
+    }
+
+
+    function getChirpsByUser(username) {
+        let endpoint = `chirps?query={"author":"${username}"}&sort={"_kmd.ect": 1}`;
+        return remote.get(appdata,endpoint,authKey)
+    }
+    
+    async function countChirp(username) {
+        return ( await getChirpsByUser(username)).length
+
+    }
+
+    //  function countChirp (username) {
+    //     return new Promise((resolve, reject) => {
+    //         remote.get('appdata', `chirps?query={"author":"${username}"}`)
+    //             .then(chirps => {
+    //             resolve(chirps.length);
+    //         }).catch(reject);
+    //     });
+    // }
+
+   async  function countFollowing(username) {
+       const currentUser  =  sessionStorage.getItem('username');
+      if(username  === currentUser){
+
+          let endpoint = `?query={"username":"${username}"}`;
+          let s =(await remote.get(user,endpoint))[0].subscriptions.length;
+          return await Number(s) ;
+
+      }
+
+    }
+   //  function getUsers () {
+   //      return new Promise((resolve, reject) => {
+   //          remote.get('user', '').then(users => {
+   //              users.map(u => u.followers = users.filter(c => c.subscriptions.includes(u.username)).length);
+   //              users.sort((a, b) => b.followers - a.followers);
+   //              resolve(users);
+   //          });
+   //      });
+   //  }
+
+    //
+    //  function countFollowing(username) {
+    //     if (username === sessionStorage.getItem('username')) {
+    //         return new Promise((resolve, reject) => {
+    //             resolve(JSON.parse(sessionStorage.getItem('subscriptions')).length);
+    //         });
+    //     } else {
+    //         return new Promise((resolve, reject) => {
+    //             remote.get('user', `?query={"username":"${username}"}`).then(users => {
+    //                 if (users.length !== 1) {
+    //                     reject('No such user');
+    //                 }
+    //                 resolve(users[0].subscriptions.length);
+    //             });
+    //         });
+    //     }
+    // }
+    //
+
+
+    async  function countFollowers(username) {
+        const currentUser  =  sessionStorage.getItem('username');
+
+        if(currentUser === username){
+
+            let endpoint = `?query={"subscriptions":"${username}"}`;
+
+            return (await remote.get(user,endpoint)).length;
+
+        }
+
+    }
+    //  function countFollowers (username) {
+    //     return remote.get('user', `?query={"subscriptions":"${username}"}`);
+    // }
+
+    async function getStats(username) {
+        let chirps = Number(await service.countChirp(username));
+        let following = Number(await service.countFollowing(username));
+        let followers = Number(await service.countFollowers(username));
+
+        return{
+            chirps,
+            following,
+            followers
+        }
+
+    }
+
+    async  function follow(targetUser) {
+        const myId = sessionStorage.getItem('userId');
+        const subscriptions  = (await remote.get(user,myId)).subscriptions || [];
+        subscriptions.push(targetUser);
+        try {
+
+            const response =  await  remote.update(user,myId,authKey,{subscriptions});
+            sessionStorage.setItem('subscriptions',JSON.stringify(response))
+        }
+        catch (e) {
+            alert(e.message)
+        }
+    }
+    async  function unfollow(targetUser) {
+        const myId = sessionStorage.getItem('userId');
+        let subscriptions  = (await remote.get(user,myId)).subscriptions || [];
+        subscriptions =  subscriptions.filter(u=> u !== targetUser);
+        try {
+            const response =  await  remote.update(user,myId,authKey,{subscriptions});
+            sessionStorage.setItem('subscriptions',JSON.stringify(response))
+        }
+        catch (e) {
+            alert(e.message)
+        }
+    }
+
+    return{
+        getChirps,
+        createChirp,
+        deleteChirp,
+        getChirpsByUser,
+        countChirp,
+        countFollowing,
+        countFollowers,
+        follow,
+        unfollow,
+        getStats
+    }
+
+})();
+
+
